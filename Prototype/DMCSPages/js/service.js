4ca544c944d200d9a882c3653984183ddd460c90
tinaxu <949330215@qq.com>
2013-03-27 17:57:40 +0800

    register and getpassword pages

--- Prototype/DMCSPages/js/service.js
+++ Prototype/DMCSPages/js/service.js
@@ -2,5 +2,8 @@ $(function(){
 		   $('#register').click(function(){
 				window.open('register.html','_self')					
 				})
+		    $('#getpassword').click(function(){
+				window.open('getpassword.html','_self')					
+				})
 		   
 		   })
