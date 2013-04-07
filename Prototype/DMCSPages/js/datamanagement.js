c8fcbd2b786d3831d5374156091f9594448135b3
tinaxu <949330215@qq.com>
2013-04-07 15:42:15 +0800

    ajax load pages

--- Prototype/DMCSPages/js/datamanagement.js
+++ Prototype/DMCSPages/js/datamanagement.js
@@ -1,9 +1,20 @@
-$(function(){
-		   $('ul.left-menu li').click(function(){
-				var This =$(this);
-				$('ul.left-menu li').removeClass('selected');					 
-						
-				This.addClass('selected');
-											   
-											   })
-		   })
+$(function() {
+
+	$('#rightContent').load('profile.html');
+
+	$('ul.left-menu li').click(function() {
+		var This = $(this);
+		$('ul.left-menu li').removeClass('selected');
+		This.addClass('selected');
+		var idStr = This.attr('id');
+		var urls = idStr + ".html";
+		$.ajax({
+			url : urls,
+			dataType : 'html',
+			success : function(data) {
+				$('#rightContent').html(data);
+			}
+		});
+
+	})
+})
