c2b4f3410479ea9bf22c74a7d65b1c1b3a21f1db
lilylong <576621903@qq.com>
2013-04-10 12:16:04 +0800

    flowinsight

--- Prototype/DMCSPages/js/flowinsight.js
+++ Prototype/DMCSPages/js/flowinsight.js
@@ -1,15 +1,29 @@
 $(function(){
 		   $('.clickdown').click(function(){
 						var This =$(this);
-						This.parent().find(".ownership").toggleClass('treeview-collapsed');
-						This.parent().find(".path").toggleClass('treeview-collapsed');
-						This.parent().find(".calculationDetail").toggleClass('treeview-collapsed');
+						This.parent().parent().find(".ownership").toggleClass('treeview-collapsed');
+						This.parent().parent().find(".path").toggleClass('treeview-collapsed');
+						This.parent().parent().find(".type1").toggleClass('treeview-collapsed');
 						var imgSrc = This.attr('src');
 					if (imgSrc.indexOf('clickdown.png') > -1) {
 						This.attr('src', 'image/clickleft.png');
 						} else {
-						This.attr('src', 'image/clickdown.png');
-						}
+								This.attr('src', 'image/clickdown.png');
+								}
 						
 										})
 		   })
+		   
+$(function(){
+		   $('#detail1').click(function(){
+						var This =$(this);
+						This.parent().parent().parent().find(".sontable").toggleClass('treeview-expanded');
+					if ($("#detail1").text() = "Detail") {
+						$("#detail1").text() = "OK"
+						} else {
+								$("#detail1").text() = "Detail";
+								}
+						
+										})
+		   })
+		   
