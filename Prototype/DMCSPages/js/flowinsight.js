d037a7075591e99fc0b425f4ec3091c7baf6afb5
tinaxu <949330215@qq.com>
2013-04-10 18:00:07 +0800

    page updated

--- Prototype/DMCSPages/js/flowinsight.js
+++ Prototype/DMCSPages/js/flowinsight.js
@@ -1,4 +1,24 @@
 $(function(){
+		   	var data = [ [ 'LED Assembly', 12 ], [ 'Other Materials', 9 ],
+			[ 'Lower Prism Film', 14 ], [ 'Bottom Diffuser', 16 ],
+			[ 'Relflective Sheet', 7 ] ];
+		var plot1 = jQuery.jqplot('chart1', [ data ], {
+		seriesDefaults : {
+			renderer : jQuery.jqplot.PieRenderer,
+			rendererOptions : {
+				showDataLabels : true
+			}
+		}
+	});
+		var plot2 = jQuery.jqplot('chart2', [ data ], {
+		seriesDefaults : {
+			renderer : jQuery.jqplot.PieRenderer,
+			rendererOptions : {
+				showDataLabels : true
+			}
+		}
+	});
+	
 		   $('.clickdown').click(function(){
 						var This =$(this);
 						This.parent().parent().find(".ownership").toggleClass('treeview-collapsed');
@@ -12,14 +32,16 @@ $(function(){
 								}
 						
 										})
-		   })
+
 		   
-$(function(){
+
 		   $('.detail1').click(function(){
 					var This =$(this);
 					This.parent().parent().parent().find(".sontable").toggleClass('treeview-expanded');
 					if (This.text() == "Detail") {
 						This.text("OK");
+						plot1.replot();
+						plot2.replot();
 						} else {
 								This.text("Detail");
 								
--- Prototype/DMCSPages/js/volumeinsight.js
+++ Prototype/DMCSPages/js/volumeinsight.js
@@ -1,30 +1,54 @@
-$(function(){
-		   $('.clickdown').click(function(){
-						var This =$(this);
-						This.parent().parent().find(".ownership").toggleClass('treeview-collapsed');
-						This.parent().parent().find(".path").toggleClass('treeview-collapsed');
-						This.parent().parent().find(".type1").toggleClass('treeview-collapsed');
-						var imgSrc = This.attr('src');
-					if (imgSrc.indexOf('clickdown.png') > -1) {
-						This.attr('src', 'image/clickleft.png');
-						} else {
-								This.attr('src', 'image/clickdown.png');
-								}
-						
-										})
-		   })
-		   
-$(function(){
-		   $('.detail1').click(function(){
-					var This =$(this);
-					This.parent().parent().parent().find(".sontable").toggleClass('treeview-expanded');
-					if (This.text() == "Detail") {
-						This.text("OK");
-						} else {
-								This.text("Detail");
-								
-								}
-						
-										})
-		   })
-		   
+$(function() {
+	var data = [ [ 'LED Assembly', 12 ], [ 'Other Materials', 9 ],
+			[ 'Lower Prism Film', 14 ], [ 'Bottom Diffuser', 16 ],
+			[ 'Relflective Sheet', 7 ] ];
+	var plot1 = jQuery.jqplot('chart1', [ data ], {
+		seriesDefaults : {
+			// Make this a pie chart.
+			renderer : jQuery.jqplot.PieRenderer,
+			rendererOptions : {
+				// Put data labels on the pie slices.
+				// By default, labels show the percentage of the slice.
+				showDataLabels : true
+			}
+		},
+		legend : {
+			show : true,
+			location : 'e'
+		}
+	});
+
+	$('.clickdown').click(
+			function() {
+				var This = $(this);
+				This.parent().parent().find(".ownership").toggleClass(
+						'treeview-collapsed');
+				This.parent().parent().find(".path").toggleClass(
+						'treeview-collapsed');
+				This.parent().parent().find(".type1").toggleClass(
+						'treeview-collapsed');
+				var imgSrc = This.attr('src');
+				if (imgSrc.indexOf('clickdown.png') > -1) {
+					This.attr('src', 'image/clickleft.png');
+				} else {
+					This.attr('src', 'image/clickdown.png');
+				}
+
+			})
+
+	$('.detail1').click(
+			function() {
+				var This = $(this);
+				This.parent().parent().parent().find(".sontable").toggleClass(
+						'treeview-expanded');
+				if (This.text() == "Detail") {
+					This.text("OK");
+					plot1.replot();
+				} else {
+					This.text("Detail");
+
+				}
+
+			})
+
+})
